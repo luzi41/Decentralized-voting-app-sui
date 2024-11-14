56b8ec17a304169b434738c960fc8e26b70c78bd
Louis Göttertz <11258028+luzi41@users.noreply.github.com>
2024-11-14 14:58:50 +0100

    Update vite.config.ts
    Switch to https.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+      host: '127.0.0.1',
+      https: {
+          cert: './127.0.0.1.crt',
+          key: './dvs.key'
+      },
+  },
+  plugins: [react(), mkcert()],
 })
